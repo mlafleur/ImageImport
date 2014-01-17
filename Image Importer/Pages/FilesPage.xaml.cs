ae8a4b89d41cf8e6f84ad79850d8ab1b1fca21c0
mlafleur <marc@massivescale.com>
2014-01-17 10:12:33 -0500

    Publish to store

--- Image Importer/Pages/FilesPage.xaml.cs
+++ Image Importer/Pages/FilesPage.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using Image_Importer.Common;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
@@ -24,9 +15,18 @@ namespace Image_Importer.Pages
     /// </summary>
     public sealed partial class FilesPage : Page
     {
-        private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private NavigationHelper navigationHelper;
         private GridView photoGridInstance;
+
+        private Model.DevicesViewModel.ImageDevice viewModel;
+
+        public FilesPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+        }
 
         /// <summary>
         /// This can be changed to a strongly typed view model.
@@ -37,7 +37,7 @@ namespace Image_Importer.Pages
         }
 
         /// <summary>
-        /// NavigationHelper is used on each page to aid in navigation and 
+        /// NavigationHelper is used on each page to aid in navigation and
         /// process lifetime management
         /// </summary>
         public NavigationHelper NavigationHelper
@@ -45,62 +45,10 @@ namespace Image_Importer.Pages
             get { return this.navigationHelper; }
         }
 
-        public FilesPage()
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            this.InitializeComponent();
-            this.navigationHelper = new NavigationHelper(this);
-            this.navigationHelper.LoadState += navigationHelper_LoadState;
         }
 
-        private Model.DevicesViewModel.ImageDevice viewModel;
-        
-        /// <summary>
-        /// Populates the page with content passed during navigation.  Any saved state is also
-        /// provided when recreating a page from a prior session.
-        /// </summary>
-        /// <param name="sender">
-        /// The source of the event; typically <see cref="NavigationHelper"/>
-        /// </param>
-        /// <param name="e">Event data that provides both the navigation parameter passed to
-        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
-        /// a dictionary of state preserved by this page during an earlier
-        /// session.  The state will be null the first time a page is visited.</param>
-        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
-        {
-            viewModel = (Model.DevicesViewModel.ImageDevice)e.NavigationParameter;
-            this.DefaultViewModel["Items"] = viewModel.Items;
-            this.pageTitle.Text = viewModel.Title;
-        }
-
-        #region NavigationHelper registration
-
-        /// The methods provided in this section are simply used to allow
-        /// NavigationHelper to respond to the page's navigation methods.
-        /// 
-        /// Page specific logic should be placed in event handlers for the  
-        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
-        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
-        /// The navigation parameter is available in the LoadState method 
-        /// in addition to page state preserved during an earlier session.
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            navigationHelper.OnNavigatedTo(e);
-        }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            navigationHelper.OnNavigatedFrom(e);
-        }
-
-        #endregion
-
-        private void ImportAll_Click(object sender, RoutedEventArgs e)
-        {
-            if (photoGridInstance.Items.Count == 0) return;
-            if (photoGridInstance.SelectedItems.Count == 0) photoGridInstance.SelectAll();
-            ExecuteImport();
-        }
         private async void ExecuteImport()
         {
             ImportProgressPanel.Visibility = Windows.UI.Xaml.Visibility.Visible;
@@ -143,7 +91,6 @@ namespace Image_Importer.Pages
 
                 try
                 {
-
                     if (appSettings.MoveSourceFiles)
                         await fileInfo.MoveAsync(destinationFolder, fileInfo.Name, Windows.Storage.NameCollisionOption.GenerateUniqueName);
                     else
@@ -160,10 +107,53 @@ namespace Image_Importer.Pages
             ImportProgressPanel.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ImportAll_Click(object sender, RoutedEventArgs e)
         {
-
+            if (photoGridInstance.Items.Count == 0) return;
+            if (photoGridInstance.SelectedItems.Count == 0) photoGridInstance.SelectAll();
+            ExecuteImport();
         }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            viewModel = (Model.DevicesViewModel.ImageDevice)e.NavigationParameter;
+            this.DefaultViewModel["Items"] = viewModel.Items;
+            this.pageTitle.Text = viewModel.Title;
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        ///
+        /// Page specific logic should be placed in event handlers for the
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        #endregion NavigationHelper registration
 
         private void photoGrid_Loaded(object sender, RoutedEventArgs e)
         {
